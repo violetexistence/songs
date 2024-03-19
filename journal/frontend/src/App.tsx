b2ca9a0e47083c893b6f77927c4343987d6febb6
Violet Robinweiler <vrobinweiler@salesforce.com>
2024-03-19 23:16:30 +0000

    migrate dockerfile to devcontainers

--- journal/frontend/src/App.tsx
+++ journal/frontend/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className='App-header'>
         <img src={logo} alt='logo' className='App-logo' />
-        <h1 className='App-title'>Welcome to Journal</h1>
+        <h1 className='App-title'>Welcome to Journal =)</h1>
       </header>
     </div>
   )
