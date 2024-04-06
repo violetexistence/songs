4ce8d2d792061208cddc627863c3c6307a3bc2cf
jnjnboo <jnjnboo@users.noreply.github.com>
2024-04-06 11:11:36 -0700

    Added automated test for locations

---
+++ journal/api/test/locations.test.ts
@@ -0,0 +1,99 @@
+import { describe, expect, it } from 'bun:test';
+import { locationsController } from '../src/controllers/locations';
+import { locationsSchema } from '../src/db/schema';
+import { Log } from '@otherguy/elysia-logging';
+
+
+describe('Locations', () => {
+    const api = locationsController;
+    const name = 'test create';
+
+    beforeAll(() => {
+        // Set up any necessary test data or environment
+    });
+
+    afterAll(() => {
+        // Clean up any resources used for testing
+    });
+
+    it('GET: should get all locations', async () => {
+        const response = await api.handle(new Request('http://localhost/locations')).then(res => res.json());
+
+        expect(response).toBeArray()
+    });
+
+    it('POST: should create a new location', async () => {
+        const actual = await createNewLocation(name); 
+
+        expect(actual.id).toBeNumber;
+        expect(actual.name).toBe(name);
+    });
+
+    it('DELETE: should delete a location', async () => {
+        const location = await createNewLocation(name);
+        const response = await api.handle(new Request(`http://localhost/locations/${location.id}`, {
+            method: 'DELETE'
+        }));
+
+        expect(response.status).toBe(204);
+    });
+
+    it('PUT: should update a location', async () => {
+
+        const {id, ...theRest}  = await createNewLocation(name);
+        const expected = {
+            ...theRest,
+            name: 'my new name'
+        }
+        const actual = await updateLocation(id, expected);
+
+        expect(actual.id).toBe(id);
+        expect(actual.name).toBe(expected.name);
+    });
+
+    it('GET /:id:should get a location by ID', async () => {
+        const expected = await createNewLocation(name);
+        const actual = await getPerson(expected.id);
+
+        expect(actual.id).toBe(expected.id);
+        expect(actual.name).toBe(expected.name);
+    });
+
+    const createNewLocation = async (name: string) => {
+        return await api.handle(new Request('http://localhost/locations', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: name
+            })
+        })).then(res => res.json())
+    }
+
+    const updateLocation = async (id: number, data: any) => {
+        return await api.handle(new Request(`http://localhost/locations/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })).then(res => res.json())
+    }
+
+    const getPerson = async (id: number) => {     
+        return await api.handle(new Request(`http://localhost/locations/${id}`, {
+            method: 'GET'
+        })).then(res => res.json())
+    }   
+});
+function beforeAll(arg0: () => void) {
+    console.log('Function not implemented.');
+}
+
+function afterAll(arg0: () => void) {
+    console.log('Function not implemented.');
+}
+
+
+
