04c7ef9f994539a4dc369be1e5c2cc5a4b65532d
Violet Robinweiler <vrobinweiler@salesforce.com>
2024-04-06 04:59:44 +0000

    api startup DX fixes

--- journal/api/src/db/index.ts
+++ journal/api/src/db/index.ts
@@ -2,6 +2,7 @@ import { Database } from 'bun:sqlite'
 import { drizzle } from 'drizzle-orm/bun-sqlite'
 import * as schema from './schema'
 
-const sqlite = new Database(process.env.DB_URL, {create: true})
+const dbUrl = process.env.DB_URL ?? 'journal.dev.db'
+const sqlite = new Database(dbUrl, {create: true})
 
 export const db = drizzle(sqlite, { schema })
