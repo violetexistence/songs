865d87de7e494f2394c2155f4b526143d0e04cb8
jnjnboo <jnjnboo@users.noreply.github.com>
2024-04-07 12:40:37 -0700

    added configuration and packages to enable tslint

--- journal/frontend/README.md
+++ journal/frontend/README.md
@@ -4,7 +4,10 @@
 ```bash
 npm install && npm run dev
 ```
-
+## Run ESLint
+``` bash
+npx eslint .
+```
 ## Customize Environment
 ```
 echo $'VITE_PORT=8888\nVITE_API_LOCATION=http://localhost:3333' >> ./.env
---
+++ journal/frontend/eslint.config.mjs
@@ -0,0 +1,48 @@
+import eslint from '@eslint/js';
+import functional from 'eslint-plugin-functional';
+import esImport from 'eslint-plugin-import'; 
+import reactPlugin from 'eslint-plugin-react';
+import hooksPlugin from 'eslint-plugin-react-hooks';
+import ts from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+// https://typescript-eslint.io/
+
+export default [
+  {
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      functional,
+      import: esImport,
+      react: reactPlugin,
+      '@typescript-eslint': ts,
+      'react-hooks': hooksPlugin,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    files: ["src/**/*.js", "src/**/*.jsx", "src/**/*.ts", "src/**/*.tsx", "src/**/*.mjs", "src/**/*.md"],
+    ignores: ["**/*.config.js", "!**/eslint.config.js", '.next/*'],
+    // flat config approach https://github.com/vercel/next.js/discussions/49337
+    rules: {
+      ...eslint.configs.recommended.rules, //https://eslint.org/docs/latest/rules/
+      //...hooksPlugin.configs.recommended.rules,
+      //...reactPlugin.configs.recommended.rules,
+      //...reactPlugin.configs['jsx-runtime'].rules,
+      ...ts.configs['eslint-recommended'].rules,
+      //...ts.configs['recommended'].rules,
+      //...ts.configs['recommended-requiring-type-checking'].rules,
+    },
+  },
+];
