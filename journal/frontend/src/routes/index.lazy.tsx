aca225f348dde23d3a89f5bc4fd11726fe810fc5
Violet Robinweiler <vrobinweiler@salesforce.com>
2024-04-08 20:19:56 +0000

    removing add button from home page

--- journal/frontend/src/routes/index.lazy.tsx
+++ journal/frontend/src/routes/index.lazy.tsx
@@ -1,10 +1,18 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import { useNavActions } from '../features/nav/useNavActions'
+import { useLayoutEffect } from 'react'
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
 })
 
 function Index() {
+  const { setNavActions } = useNavActions()
+
+  useLayoutEffect(() => {
+    setNavActions(<></>)
+  },[])
+
   return (
     <div className="p-2">
       <h3>Welcome Home =)</h3>
