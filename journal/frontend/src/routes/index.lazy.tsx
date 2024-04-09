ab60b54f1c1215f69aadda48daed1b03966c49bf
Violet Robinweiler <vrobinweiler@salesforce.com>
2024-04-09 00:00:05 +0000

    where we left off

--- journal/frontend/src/routes/index.lazy.tsx
+++ journal/frontend/src/routes/index.lazy.tsx
@@ -1,13 +1,35 @@
 import { createLazyFileRoute } from '@tanstack/react-router'
+import React, { ChangeEvent, Children, useRef, useState } from 'react'
 
 export const Route = createLazyFileRoute('/')({
   component: Index,
 })
 
 function Index() {
+  const [value, setValue] = useState('Hello!')
+  const inputValue = useRef('')
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    inputValue.current = e.target.value
+  }
+
+  const handleChangeClick = () => {
+    setValue(inputValue.current)
+  }
+
   return (
-    <div className="p-2">
-      <h3>Welcome Home =)</h3>
+    <div>
+      <input ref={inputRef} type='text' onChange={handleInputChange} />
+      <button onClick={handleChangeClick}>Set Value</button>      
+      <button onClick={() => setValue(v => v + '!')}>Add Emphasis</button>
+      <button onClick={() => inputRef.current?.focus()}>Set Focus</button>
+      <p>
+        The current value is: { value }
+      </p>
+      <p>
+        The current input value is: {inputValue.current}
+      </p>
     </div>
   )
 }
